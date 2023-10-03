c3ff478a94e9b5b9b54c2addef9bbcbfb4ced43c
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-04 01:22:00 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,16 +23,16 @@ function Hero() {
         className="absolute top-0 left-0 w-full h-full bg-black opacity-50"
       ></div>
     <div className='container-wrapper relative z-10 pt-6'>
-  <div className="flex flex-col items-center justify-center h-screen ">
-      <h1 className=" max-w-4xl tracking-wide text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-center px-4 font-car text-white text-s">
+  <div className="flex flex-col items-center justify-center h-screen absolute left-[18%] top-[9rem] ">
+      <h1 className=" max-w-4xl tracking-wide main-t text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-center px-4 font-car text-white text-s">
        Worlds First Dertiatives Trading Platform
       </h1>
    
       <div className="flex flex-col sm:flex-row items-center gap-7">
-      <a href="#" className="text-white shadow-lg  uppercase font-bold stroke-black  bg-pink px-12 py-3 rounded-full   hover:text-gray-200">
+      <a href="#" className="text-white shadow-lg  uppercase font-bold stroke-black  bg-pink px-20 py-3 rounded-full   hover:text-gray-200">
                 Presale
               </a>
-              <a href="#" className="text-white shadow-lg  uppercase font-bold stroke-black  bg-btn2 px-12 py-3 rounded-full   hover:text-gray-200">
+              <a href="#" className="text-white shadow-lg  uppercase font-bold stroke-black  bg-pink px-12 py-3 rounded-full   hover:text-gray-200">
                Join Community
               </a>
       </div>
