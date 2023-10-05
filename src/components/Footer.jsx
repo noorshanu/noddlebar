993f9d071130a504e5424169e56648389a027abd
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-06 02:46:02 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {FaSquareTwitter,FaTelegram} from 'react-icons/fa6'
 import {AiFillFacebook} from 'react-icons/ai'
 import {IoLogoDiscord} from 'react-icons/io5'
+import {BsInstagram} from 'react-icons/bs'
 
 function Footer() {
   return (
@@ -13,9 +14,9 @@ function Footer() {
 
 </div>
 <div className='flex items-center gap-4 my-2'>
-  <a href="/" className='  text-3xl text-[#f04451] hover:text-black'><FaSquareTwitter/></a>
-  <a href="/" className=' text-3xl text-[#f04451] hover:text-black'><AiFillFacebook/></a>
-  <a href="/" className=' text-2xl text-[#f04451] hover:text-black'><FaTelegram/></a>
+  <a href="https://x.com/noodleficoin" target='_blank' className='  text-3xl text-[#f04451] hover:text-black'><FaSquareTwitter/></a>
+  <a href="https://instagram.com/noodleficoin" className=' text-3xl text-[#f04451] hover:text-black'><BsInstagram/></a>
+  <a href="https://t.me/noodlefi" target='_blank' className=' text-2xl text-[#f04451] hover:text-black'><FaTelegram/></a>
   <a href="/" className=' text-3xl text-[#f04451] hover:text-black'><IoLogoDiscord/></a>
 
 </div>
